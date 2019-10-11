320500cb4568a6a3e2e2c172eb76054171e83264
Martin Vidner <mvidner@suse.cz>
2019-10-11 11:16:35 +0200

    Smoke test for mount_specials_in_destdir

--- test/root_part_test.rb
+++ test/root_part_test.rb
@@ -5,4 +5,16 @@ require_relative "test_helper"
 Yast.import "RootPart"
 
 describe Yast::RootPart do
+  describe "#mount_specials_in_destdir" do
+    before do
+      allow(subject).to receive(:MountPartition).and_return(nil)
+      allow(subject).to receive(:AddMountedPartition)
+      expect(File).to receive(:exist?).and_return(true)
+      allow(Yast::SCR).to receive(:Execute).with(Yast::Path, Array, String)
+    end
+
+    it "does not crash" do
+      expect { subject.mount_specials_in_destdir }.to_not raise_error
+    end
+  end
 end
