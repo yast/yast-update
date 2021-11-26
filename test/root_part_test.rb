1e2ad278cf0f4d773f44bad390165e1ae0393b27
David Díaz González <dgonzalez@suse.de>
2021-11-26 19:06:45 +0000

    Reintroduce some unit tests

--- test/root_part_test.rb
+++ test/root_part_test.rb
@@ -5,6 +5,213 @@ require_relative "test_helper"
 Yast.import "RootPart"
 
 describe Yast::RootPart do
+  describe "#MountFSTab" do
+    before do
+      stub_storage(scenario)
+      allow(subject).to receive(:FsckAndMount)
+      allow(Yast::UI).to receive(:OpenDialog)
+      allow(Yast::UI).to receive(:UserInput).twice.and_return(user_input, :ok)
+      # Mock the system lookup executed as last resort when the devicegraph
+      # doesn't contain the searched information
+      allow(Y2Storage::BlkDevice).to receive(:find_by_any_name)
+    end
+
+    let(:user_input) { :cancel }
+
+    let(:message) { Yast.arg_ref("") }
+
+    let(:scenario) { "two-disks-two-btrfs.xml" }
+
+    let(:fstab_sda1) do
+      [
+        {
+          "file" => "/", "mntops" => "defaults", "vfstype" => "btrfs",
+          "spec" => root_spec
+        },
+        {
+          "file" => "/lib/machines", "mntops" => "subvol=/@/lib/machines", "vfstype" => "btrfs",
+          "spec" => root_spec
+        },
+        {
+          "file" => "/var", "mntops" => "subvol=/@/var", "vfstype" => "btrfs",
+          "spec" => root_spec
+        },
+        {
+          "file" => "swap", "mntops" => "defaults", "vfstype" => "swap",
+          "spec" => "/dev/disk/by-uuid/a62e32ec-f58d-4bff-941b-6fb9ea45c016"
+        }
+      ]
+    end
+
+    let(:fstab_sda2) do
+      [
+        {
+          "file" => "/", "mntops" => "defaults", "vfstype" => "btrfs",
+          "spec" => root_spec
+        },
+        {
+          "file" => "/usr/local", "mntops" => "subvol=/@/usr/local", "vfstype" => "btrfs",
+          "spec" => root_spec
+        },
+        {
+          "file" => "/tmp", "mntops" => "subvol=/@/tmp", "vfstype" => "btrfs",
+          "spec" => root_spec
+        },
+        {
+          "file" => "/.snapshots", "mntops" => "subvol=/@/.snapshots", "vfstype" => "btrfs",
+          "spec" => root_spec
+        },
+        {
+          "file" => "swap", "mntops" => "defaults", "vfstype" => "swap",
+          "spec" => "UUID=a62e32ec-f58d-4bff-941b-6fb9ea45c016"
+        }
+      ]
+    end
+
+    context "mounting /var" do
+      before do
+        allow(subject).to receive(:FsckAndMount).with("/var", any_args)
+          .and_return(fsck_and_mount_result)
+      end
+
+      RSpec.shared_examples "mounting /var fails" do
+        context "fails with an error message" do
+          let(:fsck_and_mount_result) { "an error while mounting" }
+
+          it "displays a dialog informing the user about it" do
+            expect(Yast::UI).to receive(:OpenDialog) do |content|
+              label = content.nested_find { |e| e.is_a?(Yast::Term) && e.value == :Label }
+              text = label.params.first
+
+              expect(text).to include(var_spec)
+              expect(text).to include("could not be mounted")
+              expect(text).to include(fsck_and_mount_result)
+            end
+
+            subject.MountFSTab(fstab, message)
+          end
+
+          context "but the user decides to continue anyway" do
+            let(:user_input) { :cont }
+
+            it "returns true" do
+              result = subject.MountFSTab(fstab, message) 
+              expect(result).to eq(true)
+            end
+          end
+
+          context "and the user decides to cancel" do
+            let(:user_input) { :cancel }
+
+            it "returns false" do
+              result = subject.MountFSTab(fstab, message) 
+              expect(result).to eq(false)
+            end
+          end
+
+          context "and the user decides to check or fix the mount options" do
+            let(:user_input) { :cmd }
+
+            it "displays the mount options dialog" do
+              expect(Yast::UI).to receive(:OpenDialog) # Let's skip the first dialog
+              expect(Yast::UI).to receive(:OpenDialog) do |content|
+                heading = content.nested_find { |e| e.is_a?(Yast::Term) && e.value == :Heading }
+                text = heading.params.first
+
+                expect(text).to include("Mount Options")
+              end
+
+              subject.MountFSTab(fstab, message)
+            end
+          end
+        end
+      end
+
+      RSpec.shared_examples "mounting /var succeeds" do
+        context "and mounting /var succeeds" do
+          let(:fsck_and_mount_result) { nil }
+
+          it "returns true" do
+            result = subject.MountFSTab(fstab, message) 
+            expect(result).to eq(true)
+          end
+        end
+      end
+
+      context "if there is no separate partition" do
+        context "and no @/var subvolume" do
+          let(:fstab) { fstab_sda2 }
+          let(:root_spec) { "UUID=d6e5c710-3067-48de-8363-433e54a9d0b5" }
+
+          pending
+        end
+
+        context "and there is a @/var subvolume" do
+          let(:fstab) { fstab_sda1 }
+          let(:root_spec) { "UUID=0a0ebfa7-e1a8-45f2-ad53-495e192fcc8d" }
+
+          pending
+        end
+      end
+
+      context "if /var is a separate partition" do
+        let(:fstab) do
+          fstab_sda2 + [
+            {
+              "file" => "/var", "mntops" => "defaults", "vfstype" => "xfs",
+              "spec" => var_spec
+            }
+          ]
+        end
+
+        context "and the device is found in the system" do
+          let(:root_spec) { "UUID=d6e5c710-3067-48de-8363-433e54a9d0b5" }
+
+          let(:var_spec) { "UUID=c9510dc7-fb50-4f7b-bd84-886965c821f6" }
+
+          include_examples "mounting /var succeeds"
+        end
+
+        context "and the device is not found in the system" do
+          let(:root_spec) { "/dev/sda2" }
+
+          let(:var_spec) { "/dev/sdc1" }
+
+          include_examples "mounting /var fails"
+         end
+      end
+
+      context "if /var is a separate LVM logical volume" do
+        let(:scenario) { "trivial-lvm.yml" }
+
+        let(:fstab) do
+          fstab_sda2 + [
+            {
+              "file" => "/var", "mntops" => "defaults", "vfstype" => "xfs",
+              "spec" => var_spec
+            }
+          ]
+        end
+
+        context "and the LVM logical volume is found in the system" do
+          let(:root_spec) { "/dev/vg0/root" }
+
+          let(:var_spec) { "/dev/disk/by-uuid/4b85-3de0" }
+
+          include_examples "mounting /var succeeds"
+        end
+
+        context "and the LVM logical volume is not found in the system" do
+          let(:root_spec) { "/dev/vg0/root" }
+
+          let(:var_spec) { "/dev/disk/by-uuid/not-found" }
+
+          include_examples "mounting /var fails"
+        end
+      end
+    end
+  end
+
   describe "#mount_specials_in_destdir" do
     before do
       allow(subject).to receive(:MountPartition).and_return(nil)
